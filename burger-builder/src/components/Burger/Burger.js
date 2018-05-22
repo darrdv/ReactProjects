bad224caac56b304c999b5964790babcff2b4414
darrdv <darr.dv@gmail.com>
2018-05-21 23:55:07 -0400

    consolidating ingredients into one array

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
 
   //convert ingredients object held in state to an array
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
 
   //We use map to build the appropriate number of elements in our array
   //dynamically
@@ -13,8 +13,16 @@ const burger = (props) => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
 
+  if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
+  
+  console.log(transformedIngredients);
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
